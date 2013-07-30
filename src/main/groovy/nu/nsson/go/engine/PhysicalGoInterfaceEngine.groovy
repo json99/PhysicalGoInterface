96903dacee0b82b1c8c4103084745ed4855bf0a5
Daniel By <daniel.joh@nsson.nu>
2013-07-30 13:48:33 +0200

    Now finding board edges

--- src/main/groovy/nu/nsson/go/engine/PhysicalGoInterfaceEngine.groovy
+++ src/main/groovy/nu/nsson/go/engine/PhysicalGoInterfaceEngine.groovy
@@ -1,7 +1,6 @@
 package nu.nsson.go.engine
 
 import java.awt.image.BufferedImage;
-
 import net.sf.gogui.gtp.GtpClient.IOCallback
 import nu.nsson.go.Main;
 import nu.nsson.go.camera.WebCamWrapper
@@ -13,6 +12,7 @@ import org.opencv.core.CvType;
 import org.opencv.core.Mat
 import org.opencv.core.MatOfByte
 import org.opencv.core.MatOfPoint
+import org.opencv.core.MatOfPoint2f
 import org.opencv.core.Point
 import org.opencv.core.Scalar
 import org.opencv.core.Core
@@ -240,10 +240,14 @@ class PhysicalGoInterfaceEngine implements WebCamWrapperUpdate, IOCallback {
 		
 		Imgproc.HoughLines(currentImage, lines, 1, Math.PI / 180, 200)
 		
+		mergeRelatedLines(lines, currentImage)
+		
 		for(int i = 0; i < lines.cols(); i++) {
 			def line = lines.get(0, i)
 			drawLine(line, currentImage, new Scalar(128))
 		}
+		
+		currentImage = findBoardLines(lines, currentImage)
 		
 		updateGui()
 	}
@@ -261,5 +265,274 @@ class PhysicalGoInterfaceEngine implements WebCamWrapperUpdate, IOCallback {
 		{
 			Core.line(img, new Point(line[0], 0), new Point(line[0], img.size().height), rgb)
 		}
+	}
+	
+	def mergeRelatedLines(lines, img)
+	{
+		def current;
+		def points
+		for(int i = 0;i < lines.cols();i++)
+		{
+			current = lines.get(0, i)
+			
+			// Already used line
+			if(current[0] == 0 && current[1]==-100)
+				continue
+	
+			def p1 = current[0]
+			def theta1 = current[1]
+	
+			def pt1current = new Point()
+			def pt2current = new Point()
+			if(theta1 > Math.PI * 45 / 180 && theta1 < Math.PI * 135 / 180)
+			{
+				pt1current.x = 0
+				pt1current.y = p1 / Math.sin(theta1)
+	
+				pt2current.x = img.size().width
+				pt2current.y = -pt2current.x / Math.tan(theta1) + p1 / Math.sin(theta1)
+			}
+			else
+			{
+				pt1current.y = 0
+				pt1current.x = p1 / Math.cos(theta1)
+	
+				pt2current.y = img.size().height
+				pt2current.x = -pt2current.y / Math.tan(theta1) + p1 / Math.cos(theta1)
+			}
+	
+			def	pos
+			for(int j = 0; j < lines.cols(); j++)
+			{
+				pos = lines.get(0, j)
+				
+				if(current == pos)
+					continue
+	
+			
+				if(Math.abs(pos[0]-current[0]) < 20 && Math.abs(pos[1]-current[1]) < Math.PI * 10 / 180)
+				{
+					float p = pos[0]
+					float theta = pos[1]
+	
+					def pt1 = new Point()
+					def pt2 = new Point()
+					if(pos[1] > Math.PI * 45 / 180 && pos[1] < Math.PI * 135 / 180)
+					{
+						pt1.x = 0
+						pt1.y = p / Math.sin(theta)
+	
+						pt2.x = img.size().width
+						pt2.y = -pt2.x / Math.tan(theta) + p / Math.sin(theta)
+					}
+					else
+					{
+						pt1.y = 0
+						pt1.x = p / Math.cos(theta)
+	
+						pt2.y = img.size().height
+						pt2.x = -pt2.y / Math.tan(theta) + p / Math.cos(theta)
+					}
+	
+					def first = ((pt1.x-pt1current.x) * (pt1.x-pt1current.x) + (pt1.y-pt1current.y) * (pt1.y-pt1current.y) < 64*64)
+					def last = ((pt2.x-pt2current.x) * (pt2.x-pt2current.x) + (pt2.y-pt2current.y) * (pt2.y-pt2current.y) < 64*64)
+					
+					if(first && last)
+					{
+						current[0] = (current[0]+pos[0])/2
+						current[1] = (current[1]+pos[1])/2
+	
+						pos[0]=0
+						pos[1]=-100
+					}
+				}
+			}
+		}
+	}
+	
+	def findBoardLines(lines, img) {
+		def topEdge = new Point(100000, 100000)
+		def topYIntercept = 100000
+		def topXIntercept = 0
+		
+		def bottomEdge = new Point(-100000, -100000)
+		def bottomYIntercept = 0
+		def bottomXIntercept = 0
+		
+		def leftEdge = new Point(100000, 100000)
+		def leftXIntercept = 100000
+		def leftYIntercept = 0
+		
+		def rightEdge = new Point(-100000, -100000)
+		def rightXIntercept = 0
+		def rightYIntercept = 0
+		
+		for(int i = 0;i<lines.cols();i++)
+		{
+			def current = lines.get(0, i)
+	
+			def p = current[0]
+			def theta = current[1]
+	
+			if(p == 0 && theta == -100)
+				continue
+			
+			def xIntercept = p / Math.cos(theta)
+			def yIntercept = p / (Math.cos(theta) * Math.sin(theta))
+	
+			if(theta > Math.PI * 80 / 180 && theta < Math.PI * 100 / 180)
+			{
+				if(p < topEdge.x)
+					topEdge = new Point(current[0], current[1])
+	
+				if(p > bottomEdge.x)
+					bottomEdge = new Point(current[0], current[1])
+	
+			}
+			else if(theta < Math.PI * 10 / 180 || theta > Math.PI * 170 / 180)
+			{
+				if(xIntercept > rightXIntercept)
+				{
+					rightEdge = new Point(current[0], current[1])
+					rightXIntercept = xIntercept
+				}
+				else if(xIntercept <= leftXIntercept)
+				{
+					leftEdge = new Point(current[0], current[1])
+					leftXIntercept = xIntercept
+				}
+			}
+		}
+	
+		def left1 = new Point()
+		def left2 = new Point()
+		def right1 = new Point()
+		def right2 = new Point()
+		def bottom1 = new Point()
+		def bottom2 = new Point()
+		def top1 = new Point()
+		def top2 = new Point()
+	
+		int height=img.size().height
+		int width=img.size().width
+	
+		if(leftEdge.y != 0)
+		{
+			left1.x = 0		
+			left1.y = leftEdge.x / Math.sin(leftEdge.y)
+			left2.x = width
+			left2.y = -left2.x / Math.tan(leftEdge.y) + left1.y
+		}
+		else
+		{
+			left1.y = 0
+			left1.x = leftEdge.x / Math.cos(leftEdge.y)
+			left2.y = height
+			left2.x = left1.x - height * Math.tan(leftEdge.y)
+		}
+	
+		if(rightEdge.y != 0)
+		{
+			right1.x = 0
+			right1.y = rightEdge.x / Math.sin(rightEdge.y)
+			right2.x = width
+			right2.y = -right2.x / Math.tan(rightEdge.y) + right1.y
+		}
+		else
+		{
+			right1.y = 0
+			right1.x = rightEdge.x / Math.cos(rightEdge.y)
+			right2.y = height
+			right2.x = right1.x - height * Math.tan(rightEdge.y)
+		}
+	
+		bottom1.x = 0
+		bottom1.y = bottomEdge.x / Math.sin(bottomEdge.y)
+		bottom2.x = width
+		bottom2.y = -bottom2.x / Math.tan(bottomEdge.y) + bottom1.y
+	
+		top1.x = 0
+		top1.y = topEdge.x / Math.sin(topEdge.y)
+		top2.x = width
+		top2.y = -top2.x / Math.tan(topEdge.y) + top1.y
+	
+		// Next, we find the intersection of these four lines
+		double leftA = left2.y - left1.y
+		double leftB = left1.x - left2.x
+		double leftC = leftA * left1.x + leftB * left1.y
+	 
+		double rightA = right2.y - right1.y
+		double rightB = right1.x - right2.x
+		double rightC = rightA * right1.x + rightB * right1.y
+	 
+		double topA = top2.y - top1.y
+		double topB = top1.x - top2.x
+		double topC = topA * top1.x + topB * top1.y
+	 
+		double bottomA = bottom2.y - bottom1.y
+		double bottomB = bottom1.x - bottom2.x
+		double bottomC = bottomA * bottom1.x + bottomB * bottom1.y
+	 
+		// Intersection of left and top
+		double detTopLeft = leftA * topB - leftB * topA
+		def ptTopLeft = new Point((topB * leftC - leftB * topC) / detTopLeft, (leftA * topC - topA * leftC) / detTopLeft)
+	 
+		// Intersection of top and right
+		double detTopRight = rightA * topB - rightB * topA
+		def ptTopRight = new Point((topB * rightC - rightB * topC) / detTopRight, (rightA * topC - topA * rightC) / detTopRight)
+	 
+		// Intersection of right and bottom
+		double detBottomRight = rightA * bottomB - rightB * bottomA
+		def ptBottomRight = new Point((bottomB * rightC - rightB * bottomC) / detBottomRight, (rightA * bottomC - bottomA * rightC) / detBottomRight)
+	 
+		// Intersection of bottom and left
+		double detBottomLeft = leftA * bottomB - leftB * bottomA
+		def ptBottomLeft = new Point((bottomB * leftC - leftB * bottomC) / detBottomLeft, (leftA * bottomC - bottomA * leftC) / detBottomLeft)
+	
+		Core.line(img, ptTopRight, ptTopRight, new Scalar(255), 10)
+		Core.line(img, ptTopLeft, ptTopLeft, new Scalar(255), 10)
+		Core.line(img, ptBottomRight, ptBottomRight, new Scalar(255), 10)
+		Core.line(img, ptBottomLeft, ptBottomLeft, new Scalar(255), 10)
+		
+		// Correct the perspective transform
+		int maxLength = (ptBottomLeft.x - ptBottomRight.x) * (ptBottomLeft.x - ptBottomRight.x) + (ptBottomLeft.y - ptBottomRight.y) * (ptBottomLeft.y - ptBottomRight.y)
+		
+		int temp = (ptTopRight.x - ptBottomRight.x) * (ptTopRight.x - ptBottomRight.x) + (ptTopRight.y - ptBottomRight.y) * (ptTopRight.y - ptBottomRight.y)
+		if(temp > maxLength) 
+			maxLength = temp
+	
+		temp = (ptTopRight.x - ptTopLeft.x) * (ptTopRight.x - ptTopLeft.x) + (ptTopRight.y - ptTopLeft.y) * (ptTopRight.y - ptTopLeft.y)
+		if(temp > maxLength) 
+			maxLength = temp
+	
+		temp = (ptBottomLeft.x - ptTopLeft.x) * (ptBottomLeft.x - ptTopLeft.x) + (ptBottomLeft.y - ptTopLeft.y) * (ptBottomLeft.y - ptTopLeft.y)
+		if(temp > maxLength)
+			maxLength = temp
+	
+		maxLength = Math.sqrt(maxLength)
+	
+		def margin = (maxLength / 18.0) / 2.0
+		
+		ptTopLeft.x -= margin
+		ptTopLeft.y -= margin
+		
+		ptTopRight.x += margin
+		ptTopRight.y -= margin
+		
+		ptBottomLeft.x -= margin
+		ptBottomLeft.y += margin
+		
+		ptBottomRight.x += margin
+		ptBottomRight.y += margin
+		
+		maxLength += (margin * 2)
+		
+		def src = new MatOfPoint2f(ptTopLeft, ptTopRight, ptBottomRight, ptBottomLeft)
+		def dst = new MatOfPoint2f(new Point(0,0), new Point(maxLength-1, 0), new Point(maxLength-1, maxLength-1), new Point(0, maxLength-1))
+		
+		def result = new Mat()
+		Imgproc.warpPerspective(img, result, Imgproc.getPerspectiveTransform(src, dst), new Size(maxLength, maxLength))
+		
+		return result
 	}
 }
